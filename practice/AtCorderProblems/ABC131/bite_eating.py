f91e220346fe4b62b434cf5e37a277e5a7a8b5df
rodrinxiaft <shingeki0423@gmail.com>
2023-12-04 21:24:38 +0100

    なんかコミットできてなかた

--- practice/AtCorderProblems/ABC131/bite_eating.py
+++ practice/AtCorderProblems/ABC131/bite_eating.py
@@ -1,8 +1,14 @@
 N , L = map(int,input().split())
 
-fla = [L+i-1 for i in [j for j in range(1,N+1)]]
+pie = 0
+apple = 0
 m = float('inf')
+
 for i in range(N):
-    d= abs(sum(fla[:i]+fla[i+1:])-(sum(fla)-fla[i]))
-    m = min(m,d)
-print(m)
+    pie += L+i
+    if m > abs(L+i):
+        m = abs(L+i)
+        apple = L+i
+
+ans = pie-apple
+print(ans)
